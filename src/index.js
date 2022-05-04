5d1c402dfd51e363c7a5c4c58f6d98ed872588f1
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-04 00:11:29 -0400

    link firebase

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
 
-const { Pool } = require("pg");
-const { params } = require("./db/config");
-const db = new Pool(params);
-db.connect();
+const { initializeApp } = require("firebase/app");
+const { getFirestore, doc, getDoc } = require("firebase/firestore");
+// const bodyParser = require("body-parser");
+// const { Pool } = require("pg");
+// const { params } = require("./db/config");
+// const db = new Pool(params);
+// db.connect();
 
 const app = express();
 
@@ -21,10 +23,30 @@ app.use(cors());
 app.use(helmet());
 
 /* --------------------------------- ROUTES --------------------------------- */
-const messages = require("./routes/messages");
-/* --------------------------------- ROUTES --------------------------------- */
-app.use("/api/messages", messages(db));
+// const messages = require("./routes/messages");
+// const users = require("./routes/users");
 
+/* --------------------------------- ROUTES --------------------------------- */
+// app.use("/api/messages", messages(db));
+
+/* -------------------------------- firebase -------------------------------- */
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+app.get("/", (req, res) => {
+  res.send(db);
+});
+
+/* -------------------------------- firebase -------------------------------- */
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
