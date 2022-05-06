220a3e980c1e0af8342ab59e7d5033c96e63c154
Alison Harman <alison_harman@sympatico.ca>
2022-05-06 15:36:37 -0400

    create schema for users and minicasts table

--- src/db/schema/migrations.sql
+++ src/db/schema/migrations.sql
@@ -3,15 +3,27 @@ DROP TABLE IF EXISTS minicasts CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255),
+  handle VARCHAR(255) NOT NULL,
+  about_me TEXT,
+  avatar_link VARCHAR(2803) NOT NULL,
+  active BOOLEAN DEFAULT TRUE,
   created_at timestamp NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE minicasts (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  audio_link VARCHAR(255) NOT NULL,
-  title VARCHAR(140),
+  audio_link VARCHAR(2083) NOT NULL,
+  banner_link VARCHAR(2083) NOT NULL,
+  title VARCHAR(255),
+  description TEXT,
+  num_of_views INTEGER,
+  length_in_seconds INTEGER,
+  transcription TEXT,
+  active BOOLEAN DEFAULT TRUE,
   created_at timestamp NOT NULL DEFAULT NOW()
 );
