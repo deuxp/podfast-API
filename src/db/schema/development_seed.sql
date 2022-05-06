762669fe5cb645dd4799aaa2c744fde0e024bec3
Alison Harman <alison_harman@sympatico.ca>
2022-05-06 15:41:26 -0400

    seed data for users and minicasts table

--- src/db/schema/development_seed.sql
+++ src/db/schema/development_seed.sql
@@ -1,2 +1,9 @@
--- INSERT INTO messages (body)
--- VALUES ('hello there, can anybody see me?');
+INSERT INTO users (first_name, last_name, email, handle, password, about_me, avatar_link)
+VALUES
+('Sylvia', 'Palmer', 'palmer@example.com', 'SilverTongue', '123', 'Best in the biz!', 'https://i.imgur.com/LpaY82x.png'),
+('Walter', 'White', 'ww@example.com', 'BreakingBad', '123', 'The end justifies the extreme', 'https://i.imgur.com/3tVgsra.jpg');
+
+INSERT INTO minicasts (user_id, audio_link, banner_link, title, description, num_of_views, length_in_seconds)
+VALUES
+(1, 'iwillnotputsecretkeysinGitHub@lessons.com', 'noatrealink.com', 'Lorem Ipsum is the title', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout... ', 74, 10 ),
+(2, 'iwillnotputsecretkeysinGitHub@lessons.com', 'noatrealink.com', 'Lorem Ipsum is the title', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout... ', 50, 11 );
