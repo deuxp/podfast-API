bd7378db4da8303e95a8a239bf055045a260a915
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-05 20:36:31 -0400

    simple routes for testing

---
+++ src/routes/minicasts.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (_req, res) => {
+    const Q = `SELECT * FROM messages`;
+
+    db.query(Q)
+      .then((data) => {
+        return res.json(data.rows);
+      })
+      .catch((e) => {
+        res.send(e.message);
+      });
+  });
+
+  router.post("/upload", (req, res) => {
+    try {
+      if (!req.files) {
+        res.send({
+          status: false,
+          message: "No minicast uploaded",
+        });
+      } else {
+        let minicast = req.files.minicast; // name of input field
+        // .mv() method to move the file to elsewhere on the server
+        minicast.mv(`./public/minicasts/${minicast.name}`); // uuid for storing purposes, also puts this in the mincast insert - uuid path
+        console.log(minicast.name);
+        res.send({
+          status: true,
+          message: "File is uploaded",
+          data: {
+            name: minicast.name,
+            mimetype: minicast.mimetype,
+            size: minicast.size,
+          },
+        });
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  return router;
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// `db` would be the firebase
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.send("wuddup");
