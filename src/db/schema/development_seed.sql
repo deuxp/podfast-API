9379dae99f37c6cc9be14d1590f675fbf000ce56
Alison Harman <alison_harman@sympatico.ca>
2022-05-07 19:19:58 -0400

    add seed data for tags and minicast_tags

--- src/db/schema/development_seed.sql
+++ src/db/schema/development_seed.sql
@@ -8,3 +8,22 @@ VALUES
 (1, '/assets/minicasts/This-Much-Will-Kill-You.mp3', '/assets/banners/universe.jpg', 'This amount will kill you', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout... ', 74, 10 ),
 (1, '/assets/minicasts/This-Much-Will-Kill-You-2.mp3', '/assets/banners/animals.jpg', 'This amount will kill you part 2', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout... ', 50, 11 ),
 (2, '/assets/minicasts/60SS-Stuttering.mp3', 'assets/banners/universe.jpg', '60 Second Science', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout... ', 50, 11 );
+
+INSERT INTO tags (tag)
+VALUES
+('science'),
+('music'),
+('education'),
+('news'),
+('lifestyle'),
+('trailers'),
+('comedy');
+
+INSERT INTO minicast_tags (minicast_id, tag_id)
+VALUES
+(1, 1),
+(1, 7),
+(2, 1),
+(2, 2),
+(3, 1), 
+(3, 3);
