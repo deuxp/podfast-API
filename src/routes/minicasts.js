d8c7b1ea9932d52dce51ae152664dff624730674
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-08 23:02:55 -0400

    trying to post the minicasts

--- src/routes/minicasts.js
+++ src/routes/minicasts.js
@@ -15,31 +15,50 @@ module.exports = (db) => {
   });
 
   router.post("/upload", (req, res) => {
+    console.log(req.stack);
     try {
       if (!req.files) {
         res.send({
-          status: false,
+          status: 500,
           message: "No minicast uploaded",
         });
       } else {
-        let minicast = req.files.minicast; // name of input field
-        // .mv() method to move the file to elsewhere on the server
-        minicast.mv(`./public/minicasts/${minicast.name}`); // uuid for storing purposes, also puts this in the mincast insert - uuid path
-        console.log(minicast.name);
         res.send({
-          status: true,
-          message: "File is uploaded",
-          data: {
-            name: minicast.name,
-            mimetype: minicast.mimetype,
-            size: minicast.size,
-          },
+          status: 204,
+          message: "successful post",
         });
       }
     } catch (err) {
       res.status(500).send(err);
     }
   });
+
+  // router.post("/upload", (req, res) => {
+  //   try {
+  //     if (!req.files) {
+  //       res.send({
+  //         status: false,
+  //         message: "No minicast uploaded",
+  //       });
+  //     } else {
+  //       let minicast = req.files.minicast; // name of input field
+  //       // .mv() method to move the file to elsewhere on the server
+  //       minicast.mv(`./public/minicasts/${minicast.name}`); // uuid for storing purposes, also puts this in the mincast insert - uuid path
+  //       console.log(minicast.name);
+  //       res.send({
+  //         status: true,
+  //         message: "File is uploaded",
+  //         data: {
+  //           name: minicast.name,
+  //           mimetype: minicast.mimetype,
+  //           size: minicast.size,
+  //         },
+  //       });
+  //     }
+  //   } catch (err) {
+  //     res.status(500).send(err);
+  //   }
+  // });
 
   return router;
 };
