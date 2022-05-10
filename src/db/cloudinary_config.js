b6f8bc204c338f68484b6bfce3601ec5b8752db4
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-05-09 21:08:13 -0400

    cloudinary config

---
+++ src/db/cloudinary_config.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
